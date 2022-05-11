f6fc8db20846a74afff65cc76b3f0d29e6cf8072
tshifura887 <85707650+tshifura887@users.noreply.github.com>
2022-05-11 15:12:43 +0200

    create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,26 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id]) #
+    @article = Article.find(params[:id]) #fetching each article by id
   end
+
+  def new
+    @article = Article.new #the new action instaintiates a new article , but does not save it which will be used when building a form
+  end
+
+  def create
+    @article = Article.new(article_params) #instantiates a new article with tittle and body in it
+
+    if @article.save
+      redirect_to @article #redirects the browser to the atricles page
+    else
+      render :new #redisplays the form by rendering new
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :body) #filters the params
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")ßßß
   root "articles#index"
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+  resources :articles
 end
