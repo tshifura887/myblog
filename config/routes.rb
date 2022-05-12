1d6cb364d9e0d68593d26bfd3f86cdf1a3cf93d2
tshifura887 <85707650+tshifura887@users.noreply.github.com>
2022-05-12 10:02:53 +0200

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")ßßß
   root "articles#index"
+
   resources :articles do
     resources :comments
   end
 end
+
